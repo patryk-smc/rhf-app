e84bbeafc841a9615db49b9005585feeecbbd6c8
Moshyfawn <oleg.butorov.98@gmail.com>
2022-01-27 14:48:15 +0300

    🏗️ build out a demo form

--- src/FormPage.tsx
+++ src/FormPage.tsx
@@ -1,83 +1,84 @@
-import {
-  Page,
-  Card,
-  Form,
-  FormLayout,
-  RadioButton,
-  Stack,
-} from "@shopify/polaris";
-import { Checkbox, Select, SingleChoiceList, TextField } from "./Inputs";
-import { useForm } from "react-hook-form";
-import RenderCount from "./RenderCount";
+import { Button, Card, Form, FormLayout, Page } from '@shopify/polaris'
+import type { DefaultValues, SubmitHandler, Control } from 'react-hook-form'
+import { useForm, useWatch } from 'react-hook-form'
+import { Checkbox, Select, SingleChoiceList, TextField } from './Inputs'
+import useRenders from './useRenders'
 
-export default function Page2() {
-  type FormData = {
-    name: string;
-    accounts: "disabled" | "enabled";
-    check: boolean;
-    sel: string;
-    choice: string;
-  };
+interface FormValuesPreviewProps {
+  control: Control<FormValues>
+}
 
-  const defaultValues: FormData = {
-    name: "Jaded Pixel",
-    accounts: "disabled",
-    check: true,
-    sel: "b",
-    choice: "x",
-  };
+const FormValuesPreview = ({ control }: FormValuesPreviewProps) => {
+  const formValues = useWatch({ control })
 
-  const {
-    handleSubmit,
-    reset,
-    control,
-    watch,
-    formState: { errors, isDirty },
-  } = useForm<FormData>({
+  return (
+    <Card title='Form Values Preview (For Demo purposes)' sectioned>
+      <pre>
+        <code>{JSON.stringify(formValues, null, 2)}</code>
+      </pre>
+    </Card>
+  )
+}
+
+const options = [
+  { label: 'A', value: 'a' },
+  { label: 'B', value: 'b' },
+]
+
+type Option = 'a' | 'b'
+
+interface FormValues {
+  name: string
+  accounts: 'disabled' | 'enabled'
+  check: boolean
+  sel: Option
+  choice: Option
+}
+
+const defaultValues: DefaultValues<FormValues> = {
+  name: 'Jaded Pixel',
+  accounts: 'disabled',
+  check: true,
+  sel: 'b',
+  choice: 'a',
+}
+
+const onSubmit: SubmitHandler<FormValues> = data => {
+  console.log('Send to API', data)
+  alert(JSON.stringify(data, null, 2))
+}
+
+const Page2 = () => {
+  const { control, handleSubmit, formState } = useForm<FormValues>({
     defaultValues,
+  })
+  const { isSubmitting } = formState
   const renders = useRenders()
 
   return (
-    <Page title={"Form with React Hook Form"}>
+    <Page title={`Form with React Hook Form (Renders: ${renders})`}>
       <Card sectioned>
         <Form onSubmit={handleSubmit(onSubmit)}>
-          <RenderCount />
           <FormLayout>
-            <TextField<FormData>
+            <TextField
+              minLength={10}
               control={control}
-              name="name"
-              label="Store name"
-              autoComplete="off"
+              name='name'
+              label='Store Name'
+              autoComplete='name'
             />
-
-            <Checkbox<FormData>
-              control={control}
-              label="Basic checkbox"
-              name="check"
-            />
-
-            <Select<FormData>
-              control={control}
-              label="Select label"
-              name="sel"
-              options={[
-                { label: "A", value: "a" },
-                { label: "B", value: "b" },
-              ]}
-            />
-
-            <SingleChoiceList<FormData>
-              control={control}
-              name="choice"
-              title="Choices"
-              choices={[
-                { label: "A", value: "a" },
-                { label: "B", value: "b" },
-              ]}
-            />
+            <Checkbox control={control} label='Basic checkbox' name='check' />
+            <Select control={control} label='Select label' name='sel' options={options} />
+            <SingleChoiceList control={control} name='choice' title='Choices' choices={options} />
+            <Button submit primary loading={isSubmitting}>
+              Save product
+            </Button>
           </FormLayout>
         </Form>
       </Card>
+      <FormValuesPreview control={control} />
     </Page>
-  );
+  )
 }
+
+export default Page2
